c49318e2510501e76f3c155353faed928504107a
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2018-09-30 22:16:07 +0200

    Created tests with unittest

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
+import unittest
 from mymodule import mymodule
-print(mymodule)
-print(mymodule.sum_array([1,2,3,4]))
-print(mymodule.norm_array([1,2,3,4]))
-# help(mymodule)
+
+
+class TestMyModule(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(10, mymodule.sum_array([1,2,3,4]))
+    def test_norm(self):
+        self.assertEqual((1+4+9+16)**0.5, mymodule.norm_array([1,2,3,4]))
+
+
+if __name__ == '__main__':
+    from unittest import main
+    main()
